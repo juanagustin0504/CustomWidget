ce5b7448875d0909cb150bb5a75dd35ff01715d9
juanagustin0504 <juanagustin@naver.com>
2020-12-10 11:04:32 +0900

    - add Commit Loader, Widget

--- CustomWidget/CustomWidget.swift
+++ CustomWidget/CustomWidget.swift
@@ -8,32 +8,6 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
-
 struct Commit {
     let message: String
     let author: String
@@ -43,19 +17,97 @@ struct Commit {
 struct LastCommitEntry: TimelineEntry {
     public let date: Date
     public let commit: Commit
+    
+    var relevance: TimelineEntryRelevance? {
+        return TimelineEntryRelevance(score: 10) // 0 - not important | 100 - very important
+    }
 }
 
 struct CommitLoader {
     static func fetch(completion: @escaping (Result<Commit, Error>) -> Void) {
-        let branchContentsURL = URL(string: "https://github.com/juanagustin0504/CustomWidget.git")
+        let branchContentsURL = URL(string: "https://api.github.com/repos/juanagustin0504/CustomWidget/branches/main")!
+        URLSession.shared.dataTask(with: branchContentsURL) { (data, response, error) in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            
+            let commit = getCommitInfo(fromData: data!)
+            completion(.success(commit))
+        }.resume()
+    }
+    
+    static func getCommitInfo(fromData data: Foundation.Data) -> Commit {
+        let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+        let commitParentJson = json["commit"] as! [String: Any]
+        let commitJson = commitParentJson["commit"] as! [String: Any]
+        let authorJson = commitJson["author"] as! [String: Any]
+        let message = commitJson["message"] as! String
+        let author = authorJson["name"] as! String
+        let date = authorJson["date"] as! String
+        return Commit(message: message, author: author, date: date)
     }
 }
 
-struct CustomWidgetEntryView : View {
-    var entry: Provider.Entry
+struct CommitTimeline: TimelineProvider {
+    typealias Entry = LastCommitEntry
+    
+    func placeholder(in context: Context) -> LastCommitEntry {
+        LastCommitEntry(date: Date(), commit: Commit(message: "Loading...", author: "", date: ""))
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<LastCommitEntry>) -> Void) {
+        let currentDate = Date()
+        let refreshDate = Calendar.current.date(byAdding: .minute, value: 5, to: currentDate)!
+        
+        CommitLoader.fetch { result in
+            let commit: Commit
+            if case .success(let fetchedCommit) = result {
+                commit = fetchedCommit
+            } else {
+                commit = Commit(message: "Failed to load commits", author: "", date: "")
+            }
+            let entry = LastCommitEntry(date: currentDate, commit: commit)
+            let timeline = Timeline(entries: [entry], policy: .after(refreshDate))
+            completion(timeline)
+        }
+    }
+    
+    func getSnapshot(in context: Context, completion: @escaping (LastCommitEntry) -> Void) {
+        let fakeCommit = Commit(message: "- First Commit", author: "moon-john", date: "2020-12-10")
+        let entry = LastCommitEntry(date: Date(), commit: fakeCommit)
+        completion(entry)
+    }
+}
 
+struct CommitCheckerWidgetView : View {
+    let entry: LastCommitEntry
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        VStack(alignment: .leading, spacing: 4) {
+            Text("moon-john/WidgetSample's Latest Commit")
+                .font(.system(.title3))
+                .foregroundColor(.black)
+            Text(entry.commit.message)
+                .font(.system(.callout))
+                .foregroundColor(.black)
+                .bold()
+            Text("by \(entry.commit.author) at \(entry.commit.date)")
+                .font(.system(.caption))
+                .foregroundColor(.black)
+            Text("Updated at \(Self.format(date:entry.date))")
+                .font(.system(.caption2))
+                .foregroundColor(.black)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: [.orange, .yellow]), startPoint: .top, endPoint: .bottom))
+    }
+    
+    static func format(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM-dd-yyyy HH:mm"
+        return formatter.string(from: date)
     }
 }
 
@@ -64,17 +116,18 @@ struct CustomWidget: Widget {
     let kind: String = "CustomWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            CustomWidgetEntryView(entry: entry)
+        
+        StaticConfiguration(kind: kind, provider: CommitTimeline()) { entry in
+            CommitCheckerWidgetView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Custom Widget")
+        .description("Custom Widget for Last Commit.")
     }
 }
 
 struct CustomWidget_Previews: PreviewProvider {
     static var previews: some View {
-        CustomWidgetEntryView(entry: SimpleEntry(date: Date()))
+        CommitCheckerWidgetView(entry: LastCommitEntry(date: Date(), commit: Commit(message: "previews", author: "tester", date: "2020-12-10")))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
